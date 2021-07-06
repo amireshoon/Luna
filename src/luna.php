1fdaae7dcfdcc0d0a08fa7f91f908346278ca1ad
Amirhossein Meydani <amirhwsin@outlook.com>
2021-07-06 16:57:22 +0430

    Added string between

--- src/luna.php
+++ src/luna.php
@@ -4,6 +4,7 @@ class luna {
     
     protected $front;
     protected $front_path;
+    protected $front_args;
 
     public function __construct() {
         // cons
@@ -45,7 +46,35 @@ class luna {
         $this->front = fread($handle, filesize($this->front_path));
 
         fclose($handle);
-        
+
         return true;
+    }
+
+    /**
+     * Gets string between two string
+     * 
+     * @since 1.0
+     * @param string context
+     * @param string start delimiter
+     * @param string end delimiter
+     * @return array
+     */
+    private function get_between($str, $start, $end) : array {
+        $contents = array();
+        $startDelimiterLength = strlen($start);
+        $endDelimiterLength = strlen($end);
+        $startFrom = $contentStart = $contentEnd = 0;
+        while (false !== ($contentStart = strpos($str, $start, $startFrom))) {
+            $contentStart += $startDelimiterLength;
+            $contentEnd = strpos($str, $end, $contentStart);
+
+            if (false === $contentEnd)
+                break;
+            
+            $contents[] = substr($str, $contentStart, $contentEnd - $contentStart);
+            $startFrom = $contentEnd + $endDelimiterLength;
+        }
+
+        return $contents;
     }
 }
